1bf31b656e716189d7d9aba516e2390de4121175
seacatcode <seacatcode@gmail.com>
2023-08-16 14:23:51 +0900

    Init Express

---
+++ lib-js.js
@@ -0,0 +1,11 @@
+; var seaCatCodeMiniAnalytics = (function (O) {
+
+    O.host = O.host === void 0 ? '/' : O.host;
+
+    function send() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/'); // 메소드와 주소 설정
+    }
+
+    return O;
+})(seaCatCodeMiniAnalytics === void 0 ? {} : seaCatCodeMiniAnalytics);
---
+++ server.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const PORT = process.env.PORT ?? 3000;
+
+app.set('port', PORT);
+
+app.use(express.json(), function (err, req, res, next) {
+    if (err instanceof SyntaxError) {
+        res.json({ errMsg: 'JSON Parse SyntaxError' });
+    } else {
+        next(err);
+    }
+});
+
+app.get('/lib-js.js', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '/lib-js.js'));
+});
+
+app.get('/testView.html', function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'testView.html'));
+});
+
+app.use(function (req, res, next) {
+    res.json({ errMsg: '404' });
+});
+
+app.use(function (err, req, res, next) {
+    res.json({ errMsg: '500' });
+})
+
+app.listen();
