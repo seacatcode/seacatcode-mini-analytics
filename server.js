61395f79b6a6ba8051ccb54e027abc5493171ae7
seacatcode <seacatcode@gmail.com>
2023-08-16 16:07:22 +0900

    CORS Test

--- server.js
+++ server.js
@@ -35,12 +35,20 @@ app.get('/testView.html', function (req, res, next) {
     res.sendFile(path.join(__dirname, 'testView.html'));
 });
 
+let genNumber = new Array(10).fill(0b0).map((v, idx) => String.fromCharCode('0'.charCodeAt(0) + idx));
+let genAlphabet = new Array(26).fill(0b0).map((v, idx) => String.fromCharCode('a'.charCodeAt(0) + idx));
+let genTable = [genNumber, genAlphabet].flat(1);
 app.get('/create', async (req, res, next) => {
     let d = new Date();
-    let genRandom = String(d.getFullYear()).padStart(4, '0') + String(d.getMonth() + 1).padStart(2, '0') + String(d.getDate()).padStart(2, '0');
-    // res.send((req.query.callback ?? 'callback') + '("' + genRandom + '")');
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send(genRandom);
+    let genDate = String(d.getFullYear()).padStart(4, '0') + String(d.getMonth() + 1).padStart(2, '0') + String(d.getDate()).padStart(2, '0');
+
+    let result = [];
+    while (result.length < 16) {
+        result.push(genTable[Math.floor(Math.random() * genTable.length)]);
+    }
+
+    res.setHeader('Content-TypegenDate', 'text/plain');
+    res.status(200).send(genDate.concat(result.join('')));
 });
 
 app.get('/none', (req, res, next) => {
